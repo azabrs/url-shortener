3d1e70c88d31980707bb0c5d52bd5d255de68afc
azabrs <Apukha@mail.ru>
2024-06-09 14:30:17 +0300

    add slog

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"url-shortener/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDebug = "debug"
 )
 
 
 func main(){
 
 	cfg := config.MustLoad("../config/config.yaml")
-	fmt.Println(cfg)
+	log := SetupLogger(cfg.Env)
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+}
+
+func SetupLogger(env string) *slog.Logger{
+	var log *slog.Logger
+
+	switch env{
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDebug:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return log
 }
